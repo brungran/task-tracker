ca9d850f62996fa99d362406a44f2a236b6125fc
brungran <brungran@gmail.com>
2022-10-26 01:31:23 -0300

    Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        props: {
+            title: String
+        }
+    }
+</script>
+
+<style scoped>
+    header{
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 20px;
+    }
+</style>
